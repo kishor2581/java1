703e30f70f2ad3bb0fede0ffd92885b0fe723d8d
kishor2581 <privatework278@gmail.com>
2025-04-08 11:56:36 +0530

    Update add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+package java;
+
+public class add {
+    public static void main(String[] args) {
+    int a=5,b=6;
+    int sum=a+b;
+    System.out.println("addition of two numbers" +sum);
+    
+}
+}
